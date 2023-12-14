2976799ef14271aef2cb1b00022bb48fce9bc162
Nhthuando <151205383+Nhthuando@users.noreply.github.com>
2023-12-14 19:26:02 +0700

    Update quanlimanguoncode.cpp

--- quanlimanguoncode.cpp
+++ quanlimanguoncode.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
 	cout<<"Hello";
 	char per1[50]="1.Huynh Tan Dinh";
-	char per2[50];
+	char per2[50]="2.Ngo Huu Thuan";
 	cout<<"Quan li ma nguon: "<<endl;
 	cout<<per1<<endl;
 	cout<<per2<<endl;
