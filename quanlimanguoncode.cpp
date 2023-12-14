a419e1777382ea96694dd0141e483745f37d36ed
Huỳnh Tấn Dĩnh <152977864+huynhtandinh2005@users.noreply.github.com>
2023-12-14 19:31:41 +0700

    Update quanlimanguoncode.cpp

--- quanlimanguoncode.cpp
+++ quanlimanguoncode.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-	cout<<"Hello";
+	cout<<"Hello world ";
 	char per1[50]="1.Huynh Tan Dinh";
 	char per2[50]="2.Ngo Huu Thuan";
 	cout<<"Quan li ma nguon: "<<endl;
